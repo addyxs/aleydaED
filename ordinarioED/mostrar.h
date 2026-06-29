c91a0b7f88ea5c307f1e78bae62b1663c92bce4d
Aleyda <adyelvazquez@gmail.com>
2026-06-28 22:23:38 -0600

    Create mostrar.h

---
+++ ordinarioED/mostrar.h
@@ -0,0 +1,14 @@
+#ifndef MOSTRAR_H
+#define MOSTRAR_H
+
+#include <stdio.h>
+#include "structs.h"
+
+void mostrarTodos(struct Persona *ptr);
+void mostrarCarrera(struct Persona *ptr);
+void mostrarSemestre(struct Persona *ptr);
+void mostrarCarrerasemestre(struct Persona *ptr);
+void mostrarNombre(struct Persona *ptr);
+void mostrarMatricula(struct Persona *ptr);
+
+#endif
