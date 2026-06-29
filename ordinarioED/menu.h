7d83a8de48deee25d54741f2e6aefc7c449a0085
Aleyda <adyelvazquez@gmail.com>
2026-06-29 17:57:21 -0600

    Update menu.h

--- ordinarioED/menu.h
+++ ordinarioED/menu.h
@@ -4,77 +4,71 @@
 #include <stdio.h>
 
 int menuPrincipal(void){
-
-    int opcion; 
-    printf("\nMENÚ");
+    int opcion;
+    printf("\n---MENU---");
     printf("\n1) Altas");
     printf("\n2) Bajas");
-    printf("\n3) Mostrar"); 
-    printf("\n4) Funciones especiales"); 
-    printf("\n5) Salir");
-    printf("\nSeleccione una opción: ");
-    scanf("%d", &opcion); 
-    
-    return opcion; 
-}
+    printf("\n3) Mostrar");
+    printf("\n4)Salir");
+    printf("\n\nSeleccione una opcion: ");
+    scanf("%d", &opcion);
+
+    return opcion;
+};
 
 int menuAltas(void){
-
     int opcion;
-    printf("\nALTAS");
-    printf("\n1) Registrar a una persona");
-    printf("\n2) Registrar a varias personas"); 
-    printf("\n3) Salir"); 
-    printf("\nSeleccione una opción: "); 
+    printf("\n---ALTAS---");
+    printf("\n1) Registrar una persona");
+    printf("\n2) Registrar varias personas");
+    printf("\n3) Regresar");
+    printf("\n\nSeleccione una opcion: ");
     scanf("%d", &opcion);
 
-    return opcion; 
-}
+    return opcion;
+};
 
 int menuCarreras(void){
-
     int opcion;
-    printf("Carrera: "); 
-    printf("\n1) Ingienería en desarollo de software"); 
-    printf("\n2) Ingeniería forestal"); 
-    printf("\n3) Licenciatura en administración turística");
-    printf("\n4) Licenciatura en ciencias ambientales"); 
-    printf("\n5) Licenciatura en biología");
-    printf("\nSeleccione su carrera: ");
+    printf("\n---CARRERAS---");
+    printf("\n1) Ingenieria en Desarrollo de Software");
+    printf("\n2) Ingenieria Forestal");
+    printf("\n3) Licenciatura en Administracion Turistica");
+    printf("\n4) Licenciatura en Ciencias Ambientales");
+    printf("\n5) Licenciatura en Biologia");
+    printf("\n\nSeleccione una carrera: ");
     scanf("%d", &opcion);
 
-    return opcion; 
-}
+    return opcion;
+};
 
 int menuBajas(void){
-
     int opcion;
-    printf("\nBAJAS");
-    printf("\n1) Dar de baja a una persona");
-    printf("\n2) Dar de baja a varias personas");
-    printf("\n3) Baja general");
-    printf("\n4) Salir");
-    printf("\nSeleccione una opcion: ");
-    scanf("%d",&opcion);
+    printf("\n---BAJAS---");
+    printf("\n1) Eliminar una persona");
+    printf("\n2) Eliminar varias personas");
+    printf("\n3) Eliminar todas");
+    printf("\n4) Regresar");
+    printf("\n\nSeleccione una opcion: ");
+    scanf("%d", &opcion);
 
     return opcion;
-}
+};
 
-int menuMostrar(){
-
+int menuMostrar(void){
     int opcion;
-    printf("\nMOSTRAR");
-    printf("\n1) Todas las personas");
-    printf("\n2) Carrera");
-    printf("\n3) Semestre");
-    printf("\n4) Carrera y semestre");
-    printf("\n5) Nombre");
-    printf("\n6) Matricula");
-    printf("\n7) Salir");
-    printf("\nSeleccione una opcion: ");
-    scanf("%d",&opcion);
+    printf("\n---MOSTRAR---");
+    printf("\n1) Mostrar todas las personas");
+    printf("\n2) Mostrar por carrera");
+    printf("\n3) Mostrar por semestre");
+    printf("\n4) Mostrar por carrera y semestre");
+    printf("\n5) Buscar por nombre");
+    printf("\n6) Buscar por matricula");
+    printf("\n7) Regresar");
+    printf("\n\nSeleccione una opcion: ");
+    scanf("%d", &opcion);
 
     return opcion;
-}
+};
 
 #endif
