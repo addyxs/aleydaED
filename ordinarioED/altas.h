0db5cafb2c279729b244c1e1099ab87befd72b11
Aleyda <adyelvazquez@gmail.com>
2026-06-29 17:54:01 -0600

    Update altas.h

--- ordinarioED/altas.h
+++ ordinarioED/altas.h
@@ -5,16 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "menu.h"
 #include "structs.h"
+#include "menu.h"
 
 
 struct Persona *nuevaPersona();
 struct Alumno *nuevoAlumno();
 
 int altas(struct Persona **ptr){
-    struct Persona *P = NULL; 
-    struct Alumno *A = NULL; 
+    struct Persona *P; 
+    struct Alumno *A; 
 
     int b = 1; 
 
@@ -37,32 +37,33 @@ int altas(struct Persona **ptr){
     }
 }
 
-struct Persona *nuevaPersona(){  
+struct Persona *nuevaPersona(){
     struct Persona *P;
     P = (struct Persona *)malloc(sizeof(struct Persona));
-        if (P == NULL){
-            return NULL; 
-        }
-        P->nombre = malloc(50 * sizeof(char));
-        
-        if(P->nombre == NULL){
-            free(P);
-            return NULL;
-        }
+    if(P == NULL)
+        return NULL;
+    P->nombre = (char *)malloc(50 * sizeof(char));
+    if(P->nombre == NULL){
+        free(P);
+        return NULL;
+    }
 
-        printf("\nPERSONA\n"); 
-        printf("Nombre: ");
-        scanf(" ");
-        fgets(P -> nombre, 50, stdin); 
-        printf("Edad: ");
-        scanf("%d", &P->edad); 
-        printf("Género: ");
-        scanf(" %c", &P->genero);
-        printf("Fecha de nacimiento[DD/MM/AAAA]: ");
-        scanf("%10s", P->fn); 
+    printf("\nPERSONA\n");
 
-        return P;
-} 
+    printf("Nombre: ");
+    scanf("%s", P->nombre);
+
+    printf("Edad: ");
+    scanf("%d", &P->edad);
+
+    printf("Genero (M/F): ");
+    scanf(" %c", &P->genero);
+
+    printf("Fecha de nacimiento (DD/MM/AAAA): ");
+    scanf("%10s", P->fn);
+
+    return P;
+}
 
 struct Alumno *nuevoAlumno(){
     struct Alumno *A;
@@ -71,45 +72,54 @@ struct Alumno *nuevoAlumno(){
         if (A == NULL)
             return NULL; 
 
-        printf("\nALUMNO\n");
-        printf("Matricula: "); 
-        scanf("%s", A->matricula);
-        
-        A->carrera = menuCarreras();
-        switch(A->carrera){
-            case 1:
-                strcpy(A->correo, "CA");
-                break;
-            case 2:
-                strcpy(A->correo, "IF");
-                break;
-            case 3:
-                strcpy(A->correo, "IDSSI");
-                break;
-            case 4:
-                strcpy(A->correo, "LB");
-                break;
-            case 5:
-                strcpy(A->correo, "LAT");
-                break;
-            }
-            for(int i=0; i<4; i++){
-                A->correo[i+2] = A->matricula[i];
-            }
-            for(int i=0; i<4; i++){
-                A->correo[i+6] = A->matricula[i+6];
-            }
-                A->correo[10] = '\0';
-                printf("Correo generado: %s@unsij.edu.mx\n", A->correo);
-       
-        printf("Calificaciones: \n");
-            for(int i=0; i<5; i++){
-                for(int j=0; j<5; j++){
-                    A->calif[i][j] = rand()%11;
-                    printf("%.0f ", A->calif[i][j]);
-                    }
-                printf("\n");
-            }
+        printf("\n---DATOS DEL ALUMNO---\n");
+
+    printf("Matricula: ");
+    scanf("%10s",A->matricula);
+    A->carrera = menuCarreras();
+    printf("Semestre: ");
+    scanf("%d",&A->semestre);
+
+    switch(A->carrera)
+    {
+        case 1:
+            strcpy(A->correo,"IDS");
+            break;
+
+        case 2:
+            strcpy(A->correo,"IF");
+            break;
+
+        case 3:
+            strcpy(A->correo,"LAT");
+            break;
+
+        case 4:
+            strcpy(A->correo,"LCA");
+            break;
+
+        case 5:
+            strcpy(A->correo,"LB");
+            break;
+
+        default:
+            strcpy(A->correo,"AL");
+            break;
+    }
+    strcat(A->correo,A->matricula);
+
+    printf("\nCorreo generado: %s@unsij.edu.mx\n",A->correo);
+    printf("\nCalificaciones:\n");
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            A->calif[i][j]=rand()%11;
+            printf("%.0f ",A->calif[i][j]);
+        }
+        printf("\n");
+    }
+
     return A;
 }
 
