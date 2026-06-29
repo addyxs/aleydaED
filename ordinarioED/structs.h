faf7026361a4a58df70833ca89ac31457403e3e1
Aleyda <adyelvazquez@gmail.com>
2026-06-28 22:24:20 -0600

    Create structs.h

---
+++ ordinarioED/structs.h
@@ -0,0 +1,21 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+struct Alumno{
+    char matricula[11];
+    int carrera;
+    int semestre;
+    char correo[20];
+    float calif[5][5];
+};
+
+struct Persona{
+    char *nombre;
+    int edad;
+    char genero;
+    char fn[11];
+    struct Alumno *ptrAlum;
+    struct Persona *ptrSig;
+};
+
+#endif
